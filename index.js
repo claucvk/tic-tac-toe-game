7749b9a5b91ae0d6faf02a6ea33a0a75ad292f9e
Claudia El Khatib <claudiacv22@gmail.com>
2017-08-01 13:19:01 -0400

    updating html file

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ require('./assets/scripts/index.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+function restartGame() {
+  document.location.href = '';
+}
+// Sign up/ sign in to play the game.
+
+// Click  player 1 (X) OR player 2 (O) to switch players.
+
+// Click to put the marker in the game board square chosen.
+
+// Display a notification message when there is a three markers in a row (horizontal, vertical or diagonal).
+
+// Display and update the score game and the number of ties.
+
+// Display the user total lost games.
+
+// Display the user total won games.
+
+// Display the user total ties.
+
+// Change the password.
+
+// Log out of the game.
