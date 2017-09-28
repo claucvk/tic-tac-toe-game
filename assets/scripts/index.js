0e08eadeedd21c776a12a8fe8230a22f6ef91571
Claudia El Khatib <claudiacv22@gmail.com>
2017-09-28 14:35:36 -0400

    Fixed counter and winer/tie declaration bug. Changed: markerSquare function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,33 +13,6 @@ $(() => {
   events.addHandlers()
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-require('./example')
-let win = false
-$(function newGame () {
-  $('.play-again').on('click', function (event) {
-    $('.boardGame').find('.box').html('').on('')
-    win = false
-    a = [0, 0, 0]
-    b = [0, 0, 0]
-    c = [0, 0, 0]
-    $(document).ready(function () {
-      a0()
-      a1()
-      a2()
-      b0()
-      b1()
-      b2()
-      c0()
-      c1()
-      c2()
-    })
-  })
-})
-
 // Identify Who is the turn, switch turns and place the marker in the square chosen
 const playerX = 1
 const playerO = 2
@@ -48,6 +21,48 @@ let turn = playerX
 let a = [0, 0, 0]
 let b = [0, 0, 0]
 let c = [0, 0, 0]
+
+let win = false
+let tie = false
+
+const playerXResults = $('#playerX')
+const tiesResults = $('#ties')
+const playerOResults = $('#playerO')
+const results = $('.results')
+
+let playerXScore = 0
+let playerOScore = 0
+let ties = 0
+let times = 0
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+
+// use require without a reference to ensure a file is bundled
+require('./example')
+
+$('.play-again').on('click', newGame)
+function newGame () {
+  $('.boardGame').find('.box').html('').on('')
+  win = false
+  tie = false
+  times = 0
+  turn = playerX
+  a = [0, 0, 0]
+  b = [0, 0, 0]
+  c = [0, 0, 0]
+  // $(document).ready(function () {
+  a0()
+  a1()
+  a2()
+  b0()
+  b1()
+  b2()
+  c0()
+  c1()
+  c2()
+  // })
+}
 
 const mark = function (selector, column, columnIndex) {
   $(selector).click(function () {
@@ -66,15 +81,15 @@ const mark = function (selector, column, columnIndex) {
   })
 }
 
-const a0 = () => mark('#a0', a, 0)
-const a1 = () => mark('#a1', a, 1)
-const a2 = () => mark('#a2', a, 2)
-const b0 = () => mark('#b0', b, 0)
-const b1 = () => mark('#b1', b, 1)
-const b2 = () => mark('#b2', b, 2)
-const c0 = () => mark('#c0', c, 0)
-const c1 = () => mark('#c1', c, 1)
-const c2 = () => mark('#c2', c, 2)
+const a0 = () => mark('#a0', 'a', 0)
+const a1 = () => mark('#a1', 'a', 1)
+const a2 = () => mark('#a2', 'a', 2)
+const b0 = () => mark('#b0', 'b', 0)
+const b1 = () => mark('#b1', 'b', 1)
+const b2 = () => mark('#b2', 'b', 2)
+const c0 = () => mark('#c0', 'c', 0)
+const c1 = () => mark('#c1', 'c', 1)
+const c2 = () => mark('#c2', 'c', 2)
 
 const playersTurn = $('.whosTurn')
 $(document).ready(function () {
@@ -101,60 +116,55 @@ const playerTurn = function playerTurn () {
   }
 }
 const markerSquare = function markerSquare (turn, columnName, columnIndex) {
-  columnName[columnIndex] = turn
+  if (columnName === 'a') {
+    a[columnIndex] = turn
+  } else if (columnName === 'b') {
+    b[columnIndex] = turn
+  } else if (columnName === 'c') {
+    c[columnIndex] = turn
+  }
+  console.log('markerSquare', turn, columnName, columnIndex)
   checkingResult()
 }
 
-const playerXResults = $('#playerX')
-const tiesResults = $('#ties')
-const playerOResults = $('#playerO')
-const results = $('.results')
-
-let playerXScore = 0
-let playerOScore = 0
-let ties = 0
-let times = 0
-
 // Declare winner X or O
 const onWin = (player) => {
-  if (player === 'X') {
-    playerXScore++
-    playerXResults.html(playerXScore)
-    results.html('Player X wins ')
-  } else if (player === 'O') {
-    playerOScore++
-    playerOResults.html(playerOScore)
-    results.html('Player O wins ')
+  console.log('checking win')
+  if (tie !== true) {
+    if (player === 'X') {
+      playerXScore++
+      playerXResults.html(playerXScore)
+      results.html('Player X wins ')
+    } else if (player === 'O') {
+      playerOScore++
+      playerOResults.html(playerOScore)
+      results.html('Player O wins ')
+    }
+    win = true
   }
-  win = true
 }
 
 // Declare a tie
-// const onTie = () => {
-//   if (times === 9 && win === false) {
-//     ties++
-//     tiesResults.html(ties)
-//     win = true
-//     results.html("It's a tie")
-//   }
-// }
+const onTie = () => {
+  console.log('checking tie')
+  if (win !== true) {
+    ties++
+    tiesResults.html(ties)
+    win = true
+    results.html("It's a tie")
+    tie = true
+  }
+}
 
 // Check each vertical for 3 markers in a row.
 const verticalChecking = function verticalChecking () {
   for (let i = 0; i < 3; i++) {
     if (a[i] === b[i] && b[i] === c[i] && c[i] === playerX) {
       onWin('X')
-    }
-    if (a[i] === b[i] && b[i] === c[i] && c[i] === playerO) {
+    } else if (a[i] === b[i] && b[i] === c[i] && c[i] === playerO) {
       onWin('O')
     } else if (times === 9 && win === false) {
-      ties++
-      tiesResults.html(ties)
-      win = true
-      results.html("It's a tie")
-      return
-    } else {
-      console.log('there is not 3 markers in a row')
+      onTie()
     }
   }
 }
@@ -163,28 +173,18 @@ const verticalChecking = function verticalChecking () {
 const horizontalChecking = function horizontalChecking () {
   if (a[0] === a[1] && a[1] === a[2] && a[2] === playerX) {
     onWin('X')
-  }
-  if (a[0] === a[1] && a[1] === a[2] && a[2] === playerO) {
+  } else if (a[0] === a[1] && a[1] === a[2] && a[2] === playerO) {
     onWin('O')
-  }
-  if (b[0] === b[1] && b[1] === b[2] && b[2] === playerX) {
+  } else if (b[0] === b[1] && b[1] === b[2] && b[2] === playerX) {
     onWin('X')
-  }
-  if (b[0] === b[1] && b[1] === b[2] && b[2] === playerO) {
+  } else if (b[0] === b[1] && b[1] === b[2] && b[2] === playerO) {
     onWin('O')
-  }
-  if (c[0] === c[1] && c[1] === c[2] && c[2] === playerX) {
+  } else if (c[0] === c[1] && c[1] === c[2] && c[2] === playerX) {
     onWin('X')
-  }
-  if (c[0] === c[1] && c[1] === c[2] && c[2] === playerO) {
+  } else if (c[0] === c[1] && c[1] === c[2] && c[2] === playerO) {
     onWin('O')
-  }
-  if (times === 9 && win === false) {
-    ties++
-    tiesResults.html(ties)
-    win = true
-    results.html("It's a tie")
-    return
+  } else if (times === 9 && win === false) {
+    onTie()
   }
 }
 
@@ -192,43 +192,29 @@ const horizontalChecking = function horizontalChecking () {
 const diagonalLeftChecking = function diagonalLeftChecking () {
   if (c[0] === b[1] && b[1] === a[2] && a[2] === playerX) {
     onWin('X')
-  }
-  if (c[0] === b[1] && b[1] === a[2] && a[2] === playerO) {
+  } else if (c[0] === b[1] && b[1] === a[2] && a[2] === playerO) {
     onWin('O')
   } else if (times === 9 && win === false) {
-    ties++
-    tiesResults.html(ties)
-    win = true
-    results.html("It's a tie")
-    return
-  } else {
-    console.log('there is not 3 markers in a row')
+    onTie()
   }
 }
 
 // Check diagonal starts a[0] for 3 markers in a row.
 const diagonalRightChecking = function diagonalRightChecking () {
+  console.log(a[0], b[1], c[2], turn)
   if (a[0] === b[1] && b[1] === c[2] && c[2] === playerX) {
     onWin('X')
-  }
-  if (a[0] === b[1] && b[1] === c[2] && c[2] === playerO) {
+  } else if (a[0] === b[1] && b[1] === c[2] && c[2] === playerO) {
     onWin('O')
   } else if (times === 9 && win === false) {
-    ties++
-    tiesResults.html(ties)
-    win = true
-    results.html("It's a tie")
-    return
-  } else {
-    console.log('there is not 3 markers in a row')
+    onTie()
   }
 }
-
 function checkingResult () {
-  console.log(times)
-  console.log(win)
-  console.log(turn)
-  console.log(ties)
+  console.log('times ', times)
+  console.log('win ', win)
+  console.log('turn ', turn)
+  console.log('ties ', ties)
   times++
   verticalChecking()
   horizontalChecking()
@@ -236,7 +222,7 @@ function checkingResult () {
   diagonalRightChecking()
   playerTurn()
   if (win === true) {
-    console.log('hola')
+    console.log('checking if won')
     times = 0
   }
   console.log(a)
